5dba7311820d237dad9e23d1303aab4747fdd42e
amberlynn <amberlynn364@gmail.com>
2023-10-30 01:26:29 +0300

    refactor: improve DataCard with CardDescription and sass mixins

--- src/components/DataCard/DataCard.tsx
+++ src/components/DataCard/DataCard.tsx
@@ -1,27 +1,22 @@
 import { Component } from 'react';
 import styles from './DataCard.module.scss';
 import { DataCardProps } from './DataCardTypes';
+import CardDescription from './CardDescription/CardDescription';
 
 export default class DataCard extends Component<DataCardProps> {
   render() {
     const { data, isLoading } = this.props;
+    if (!data) return null;
     if (isLoading) return <p>Loading data...</p>;
-    if (data && data.results.length === 0) {
+    if (data.results.length === 0) {
       return <p>Character with this name was not found</p>;
     }
     return (
       data && (
-        <ul className={styles['card-list']}>
+        <ul className={styles.cardList}>
           {data.results.map((character) => (
             <li key={character.name} className={styles.card}>
-              <h3 className={styles['card-title']}>{character.name}</h3>
-              <div className={styles['card-description']}>
-                <span>Eye color: {character.eye_color}</span>
-                <span>Gender: {character.gender}</span>
-                <span>Hair color: {character.hair_color}</span>
-                <span>Height: {character.height}</span>
-                <span>Mass: {character.mass}</span>
-              </div>
+              <CardDescription character={character} />
             </li>
           ))}
         </ul>
