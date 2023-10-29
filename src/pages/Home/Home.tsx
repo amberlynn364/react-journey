7f48abc49c6eb94262536699a9c6e63c6a7ac51e
amberlynn <amberlynn364@gmail.com>
2023-10-29 22:30:52 +0300

    refactor: remove code dublication

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,7 +15,15 @@ export default class Home extends Component {
     hasError: false,
   };
 
-  async componentDidMount(): Promise<void> {
+  componentDidMount(): void {
+    this.handleDataFetch();
+  }
+
+  setSearchValue = (newValue: string): void => {
+    this.setState({ searchValue: newValue });
+  };
+
+  handleDataFetch = async () => {
     const { searchValue } = this.state;
     this.setState({ isLoading: true });
     try {
@@ -26,44 +34,17 @@ export default class Home extends Component {
     } finally {
       this.setState({ isLoading: false });
     }
-  }
-
-  setSearchValue = (newValue: string): void => {
-    this.setState({ searchValue: newValue });
   };
 
   sendSearchValue = async (): Promise<void> => {
     const { searchValue } = this.state;
     localStorage.setItem('searchValue', searchValue);
-    this.fetchData(searchValue);
+    this.handleDataFetch();
   };
 
   throwError = (): void => {
     this.setState({ hasError: true });
   };
-
-  async fetchData(value?: string) {
-    const { searchValue } = this.state;
-    let apiUrl = localStorage.getItem('searchValue')
-      ? `https://swapi.dev/api/people/?search=${searchValue}`
-      : 'https://swapi.dev/api/people/';
-
-    if (value) apiUrl = `https://swapi.dev/api/people/?search=${value}`;
-    this.setState({ isLoading: true });
-
-    try {
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      this.setState({ data });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
 
   render() {
     const { data, searchValue, isLoading, hasError } = this.state;
