f4bae934910fd2177ecba5e06a47a56d87a07814
amberlynn <amberlynn364@gmail.com>
2023-10-30 17:59:54 +0300

    refactor: migrate SearchBar from scss to scss.module

--- src/components/View/SearchBar/SearchBar.tsx
+++ src/components/View/SearchBar/SearchBar.tsx
@@ -1,12 +1,14 @@
 import { ChangeEvent, Component } from 'react';
 import styles from './SearchBar.module.scss';
 import { SearchBarProps } from './SearchBarTypes';
+import debounce from '../../../utils/debounce';
 
 export default class SearchBar extends Component<SearchBarProps> {
   handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
     const { onChange } = this.props;
-    onChange(newValue);
+    const debouncedOnChange = debounce<string>((value) => onChange(value), 100);
+    debouncedOnChange(newValue);
   };
 
   render() {
@@ -15,14 +17,14 @@ export default class SearchBar extends Component<SearchBarProps> {
       <div className={`${styles.form}`}>
         <input
           type="input"
-          className={styles.form__field}
+          className={styles.formField}
           placeholder="Enter character name"
           name="search-bar"
           id="search-bar"
           value={value}
           onChange={this.handleInputChange}
         />
-        <label htmlFor="search-bar" className={styles.form__label}>
+        <label htmlFor="search-bar" className={styles.formLabel}>
           Enter character name
         </label>
       </div>
