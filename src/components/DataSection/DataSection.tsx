60445d7605ea6ef342a64197330265a2105a8ea2
amberlynn <amberlynn364@gmail.com>
2023-10-31 23:00:57 +0300

    refactor: migrate DataSection class component to function component

--- src/components/DataSection/DataSection.tsx
+++ src/components/DataSection/DataSection.tsx
@@ -1,24 +1,19 @@
-import { Component } from 'react';
 import styles from './DataSection.module.scss';
 import DataCard from '../DataCard/DataCard';
 import { DataSectionProps } from './DataSectionTypes';
 import LoadingSpinner from '../View/LoadingSpinner/LoadingSpinner';
 
-export default class DataSection extends Component<DataSectionProps> {
-  render() {
-    const { data, isLoading } = this.props;
-    if (isLoading) {
-      return (
-        <section className={styles.dataSection}>
-          <LoadingSpinner />
-        </section>
-      );
-    }
-    return (
-      <section className={styles.dataSection}>
-        <h1>Star Wars Characters</h1>
-        <DataCard data={data} isLoading={isLoading} />
-      </section>
-    );
-  }
+export default function DataSection({ data, isLoading }: DataSectionProps) {
+  return (
+    <section className={styles.dataSection}>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <h1>Star Wars Characters</h1>
+          <DataCard data={data} />
+        </>
+      )}
+    </section>
+  );
 }
