8989e9683b5fc8aeca398a8d4a073a9cabc87398
amberlynn <amberlynn364@gmail.com>
2023-10-31 22:50:53 +0300

    refactor: migrate LoadingSpinner class component to function component

--- src/components/View/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/View/LoadingSpinner/LoadingSpinner.tsx
@@ -1,15 +1,12 @@
-import { Component } from 'react';
 import styles from './LoadingSpinner.module.scss';
 
-export default class LoadingSpinner extends Component {
-  render() {
-    return (
-      <div className={styles.spinner}>
-        Loading
-        <div className={`${styles.spinnerSector} ${styles.spinnerSectorRed}`} />
-        <div className={`${styles.spinnerSector} ${styles.spinnerSectorBlue}`} />
-        <div className={`${styles.spinnerSector} ${styles.spinnerSectorGreen}`} />
-      </div>
-    );
-  }
+export default function LoadingSpinner() {
+  return (
+    <div className={styles.spinner}>
+      Loading
+      <div className={`${styles.spinnerSector} ${styles.spinnerSectorRed}`} />
+      <div className={`${styles.spinnerSector} ${styles.spinnerSectorBlue}`} />
+      <div className={`${styles.spinnerSector} ${styles.spinnerSectorGreen}`} />
+    </div>
+  );
 }
