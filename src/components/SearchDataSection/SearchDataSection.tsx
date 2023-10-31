8b7d2da509c914d90fbf95556f4e614fa0185864
amberlynn <amberlynn364@gmail.com>
2023-10-31 23:02:10 +0300

    refactor: migrate SearchDataSection class component to function component

--- src/components/SearchDataSection/SearchDataSection.tsx
+++ src/components/SearchDataSection/SearchDataSection.tsx
@@ -1,19 +1,24 @@
-import { Component } from 'react';
 import styles from './SearchDataSection.module.scss';
 import SearchBar from '../View/SearchBar/SearchBar';
 import Button from '../View/Button/Button';
 import { SearchDataProps } from './SearchDataSectionTypes';
 
-export default class SearchData extends Component<SearchDataProps> {
-  render() {
-    const { searchValue, setSearchValue, sendSearchValue, isLoading } = this.props;
-    return (
-      <section className={styles['search-section']}>
-        <SearchBar value={searchValue} onChange={(newValue) => setSearchValue(newValue)} />
-        <Button onClick={sendSearchValue} disabled={isLoading}>
-          Search
-        </Button>
-      </section>
-    );
-  }
+export default function SearchData({
+  searchValue,
+  setSearchValue,
+  sendSearchValue,
+  isLoading,
+}: SearchDataProps) {
+  return (
+    <section className={styles['search-section']}>
+      <SearchBar
+        value={searchValue}
+        label="Enter character name"
+        onChange={(newValue) => setSearchValue(newValue)}
+      />
+      <Button onClick={sendSearchValue} disabled={isLoading}>
+        Search
+      </Button>
+    </section>
+  );
 }
